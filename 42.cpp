ef94da7345b2807027ffaef6f313ef40510b1538
phamnhanquyen <phamnhanquyen1202001@gmail.com>
2020-05-28 00:34:52 +0700

    first commit

---
+++ 42.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int tong(int n){
+	if(n==1) return 1;
+	return tong(n-1)+n;
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	for(int i=n;i>=0;i--){
+		if(tong(i)<n) {
+		printf("%d",i);break;
+	}}
+}
